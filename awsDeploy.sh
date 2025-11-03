bf2963e0a7b0c7afd782445b35a87882f077f8d1
Jack Wagenheim <wagenheimj@moravian.edu>
2025-11-03 17:22:39 -0500

    Test PR

--- awsDeploy.sh
+++ awsDeploy.sh
@@ -1,10 +1,14 @@
-# Install python and dependencies
-sudo yum install python3 -y
-sudo pip3 install -r requirements.txt
+#!/bin/bash
+
+# Install python
+sudo yum install python3 python3-pip git -y
 
 # Clone the repository
 git clone https://github.com/cs298f25/SpotifyWebProject.git /home/ec2-user/SpotifyWebProject
 cd /home/ec2-user/SpotifyWebProject
+
+# Install dependencies
+sudo pip install -r requirements.txt
 
 # Run the Flask application
 python3 app.py
