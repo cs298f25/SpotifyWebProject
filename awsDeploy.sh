a57c900f45279d153a9f9d0631331120f32e6441
Jack Wagenheim <wagenheimj@moravian.edu>
2025-11-10 13:01:45 -0500

    Another attempt to fix awsDeploy.sh

--- awsDeploy.sh
+++ awsDeploy.sh
@@ -3,8 +3,7 @@
 # Install python & Redis
 sudo yum update -y
 sudo yum install -y python3 python3-pip git
-sudo amazon-linux-extras install -y redis6
-sudo /home/ec2-user/SpotifyWebProject/.venv/bin/python3 -m pip install --upgrade pip
+sudo dnf install -y redis
 
 # Clone the repository
 git clone https://github.com/cs298f25/SpotifyWebProject.git /home/ec2-user/SpotifyWebProject
@@ -12,6 +11,8 @@ cd /home/ec2-user/SpotifyWebProject
 
 # Activate venv
 source ./virtualEnvironment.sh
+
+sudo python -m pip install --upgrade pip
 
 # Install and start services
 sudo cp flask.service /etc/systemd/system/
