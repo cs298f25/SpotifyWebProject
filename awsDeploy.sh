47c00f91e153d82ae0a50dc6fae68103419804f2
Jack Wagenheim <wagenheimj@moravian.edu>
2025-11-10 17:56:30 -0500

    Broke up the awsDeploy sh file into two parts. One to run before you put stuff in env, another for after.

--- awsDeploy.sh
+++ awsDeploy.sh
@@ -1,21 +1,2 @@
-#!/bin/bash
-
-# Install python & Redis
-sudo yum update -y
-sudo yum install -y python3 python3-pip git
-sudo dnf install -y redis6
-
-# Clone the repository
-git clone https://github.com/cs298f25/SpotifyWebProject.git /home/ec2-user/SpotifyWebProject
-cd /home/ec2-user/SpotifyWebProject
-
-# Activate venv
-source ./virtualEnvironment.sh
-
-sudo /home/ec2-user/SpotifyWebProject/.venv/bin/python3 -m pip install --upgrade pip
-
-# Install and start services
-sudo cp flask.service /etc/systemd/system/
-sudo systemctl daemon-reload
 sudo systemctl enable --now redis6
 sudo systemctl enable --now flask
---
+++ awsPrepare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install python & Redis
+sudo yum update -y
+sudo yum install -y python3 python3-pip git
+sudo dnf install -y redis6
+
+# Clone the repository
+git clone https://github.com/cs298f25/SpotifyWebProject.git /home/ec2-user/SpotifyWebProject
+cd /home/ec2-user/SpotifyWebProject
+
+# Activate venv
+source ./virtualEnvironment.sh
+
+sudo /home/ec2-user/SpotifyWebProject/.venv/bin/python3 -m pip install --upgrade pip
+
+# Ready services
+sudo cp flask.service /etc/systemd/system/
+sudo systemctl daemon-reload
